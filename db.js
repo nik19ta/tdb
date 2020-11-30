b0ad8d6f4340ffe014df3a6277748775b550bffd
Nikita Khvatov <44050303+nik19ta@users.noreply.github.com>
2020-11-30 06:01:04 +0300

    add update_field

--- db.js
+++ db.js
@@ -38,7 +38,14 @@ function update_obj(name, key, value, key_upt, val_upt) {
         fs.writeFileSync(filename, JSON.stringify(data));
     });
 }
-
+function update_field(name, key, value) {
+    fs.readFile(filename, encoding, (err, data) => {
+        if (err) throw err;
+        data = JSON.parse(data);
+        data[name][key]=value;
+        fs.writeFileSync(filename, JSON.stringify(data));
+    });
+}
 function select_obj(name, key, value) {
     let data = fs.readFileSync(filename, encoding);
     data = JSON.parse(data);
@@ -56,3 +63,4 @@ module.exports.create_obj = create_obj;
 module.exports.del_obj = del_obj;
 module.exports.update_obj = update_obj;
 module.exports.select_obj = select_obj;
+module.exports.update_field = update_field;
